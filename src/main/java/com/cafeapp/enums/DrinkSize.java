0001909343231071d0588729095f5a1560ff6b60
Blaine Bell <blainebell21@gmail.com>
2026-05-20 23:58:13 -0700

    created diagram for cafe application structure

--- src/main/java/com/cafeapp/enums/DrinkSize.java
+++ src/main/java/com/cafeapp/enums/DrinkSize.java
@@ -1,4 +1,22 @@
 package com.cafeapp.enums;
 
 public enum DrinkSize {
+    SMALL("Small",3.50),
+    MEDIUM("Medium",9),
+    LARGE("Large",8.50);
+
+    private final double baseCost;
+    private final String drinkSize;
+
+    DrinkSize(String drinkSize, double baseCost){
+        this.baseCost = baseCost;
+        this.drinkSize = drinkSize;
+    }
+
+    public double getBaseCost(){
+        return baseCost;
+    }
+    public String getDrinkSize(){
+        return drinkSize;
+    }
 }
